c0a9e4316e607930b782a0da5e9274d8bab61aa3
Vedarth Kulkarni <vedarthk@vedarthz.in>
2014-12-14 00:22:02 +0530

    fix small typo

--- exreporter/contrib/django_middlewares.py
+++ exreporter/contrib/django_middlewares.py
@@ -6,10 +6,10 @@ exporter.contrib.django_middlewares
 
 This module implements Django middlewares for different stores.
 Requires following Django settings to be present:
-- EXREPORTER_GIHUB_USER
-- EXREPORTER_GIHUB_REPO
-- EXREPORTER_GIHUB_AUTH_TOKEN
-- EXREPORTER_GIHUB_LABELS
+- EXREPORTER_GITHUB_USER
+- EXREPORTER_GITHUB_REPO
+- EXREPORTER_GITHUB_AUTH_TOKEN
+- EXREPORTER_GITHUB_LABELS
 
 :copyright: (c) 2014 by Vedarth Kulkarni.
 :license: MIT, see LICENSE for more details.
@@ -29,9 +29,9 @@ class ExreporterGithubMiddleware(object):
         """Report exceptions from requests via Exreporter.
         """
         credentials = GithubCredentials(
-            user=settings.EXREPORTER_GIHUB_USER,
-            repo=settings.EXREPORTER_GIHUB_REPO,
-            auth_token=settings.EXREPORTER_GIHUB_AUTH_TOKEN)
+            user=settings.EXREPORTER_GITHUB_USER,
+            repo=settings.EXREPORTER_GITHUB_REPO,
+            auth_token=settings.EXREPORTER_GITHUB_AUTH_TOKEN)
 
         exreporter.report_github_issue(
-            credentials=credentials, labels=settings.EXREPORTER_GIHUB_LABELS)
+            credentials=credentials, labels=settings.EXREPORTER_GITHUB_LABELS)
