5dd48664b7ab51f1a75f324d63e2d1ff5101b717
Vedarth Kulkarni <vedarthk@vedarthz.in>
2015-04-03 17:41:47 +0530

    update basic unit tests for issue reporter

--- tests/test_reporter.py
+++ tests/test_reporter.py
@@ -17,23 +17,23 @@ class TestReporter(unittest.TestCase):
 
     @patch('exreporter.reporter.Formats')
     @patch('exreporter.reporter.StackTrace')
-    @patch('exreporter.reporter.Stores')
-    def test_reporter_report_with_defaults(self, Stores, StackTrace, Formats):
-        Stores.some_name = store_cls = MagicMock()
-        store = store_cls()
-        store_name = 'some_name'
-        credentials = object()
+    def test_reporter_report_with_defaults(self, StackTrace, Formats):
+        store = MagicMock()
         stack_trace = StackTrace()
         stack_trace.exception = ValueError
 
-        reporter = Reporter(credentials=credentials, store_name=store_name)
+        reporter = Reporter(store=store)
 
         reporter.report()
 
         store.create_or_update_issue.assert_called_once_with(
             title=Formats.title.format(),
             culprit=Formats.culprit.format(),
-            body='''{}
+            body='''{} {}
 
 {}
-'''.format(Formats.body.format(), Formats.culprit.format()))
+'''.format(Formats.body.format(),
+           Formats.locals_format.format(),
+           Formats.culprit.format()),
+            labels=reporter.labels, time_delta=reporter.time_delta,
+            max_comments=reporter.max_comments)
